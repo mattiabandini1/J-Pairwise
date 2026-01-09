c7f6818503cefd3f42b431738e66a9b0d4564b0b
mattiabandini1 <mattia.bandini3@studio.unibo.it>
2026-01-09 20:49:09 +0100

    fix: class InputData modified

--- src/main/java/com/mattiabandini/jpairwise/model/InputData.java
+++ src/main/java/com/mattiabandini/jpairwise/model/InputData.java
@@ -2,9 +2,26 @@ package com.mattiabandini.jpairwise.model;
 
 import java.util.List;
 
-public record InputData(
-        String className,
-        String packageName,
-        List<Parameter> parameters
-) {
+public class InputData {
+
+    private String className;
+    private String packageName;
+    private String targetClassName;
+    private String targetMethodName;
+    private List<Parameter> parameters;
+
+    public String getClassName() { return this.className; }
+    public String getPackageName() { return this.packageName; }
+    public String getTargetClassName() { return this.targetClassName; }
+    public String getTargetMethodName() { return this.targetMethodName; }
+    public List<Parameter> getParameters() { return this.parameters; }
+
+    public void setClassName(String className) { this.className = className; }
+    public void setPackageName(String packageName) { this.packageName = packageName; }
+    public void setTargetClassName(String targetClassName) { this.targetClassName = targetClassName; }
+    public void setTargetMethodName(String targetMethodName) { this.targetMethodName = targetMethodName; }
+    public void setParameters(List<Parameter> parameters) { this.parameters = parameters; }
+
 }
+
+
