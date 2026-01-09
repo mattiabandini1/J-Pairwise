93d32d112eae89401be0ce4f9b7175f970ec2a5f
mattiabandini1 <mattia.bandini3@studio.unibo.it>
2026-01-09 12:29:08 +0100

    feat: first test in class Main

---
+++ src/main/java/com/mattiabandini/jpairwise/Main.java
@@ -0,0 +1,21 @@
+package com.mattiabandini.jpairwise;
+
+import com.mattiabandini.jpairwise.model.Parameter;
+import com.mattiabandini.jpairwise.model.TestCase;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Parameter p1 = new Parameter("Browser", List.of("Chrome", "Firefox"));
+        System.out.println("Parameter created:" + p1);
+
+        // Parameter pError = new Parameter("Error", List.of());
+
+        TestCase testCase = new TestCase();
+        testCase.put("Browser", "Chrome");
+        testCase.put("OS", "Windows");
+        System.out.println("Test case created:" + testCase);
+        System.out.println("OS value: " + testCase.get("OS"));
+    }
+}
